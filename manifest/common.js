196b22e50703a9aa78a5e6a68bbd9fa7e1cfd465
monty68 <monty-68@hotmail.com>
2017-07-02 11:03:47 +0100

    Remove bad CSS function

--- manifest/common.js
+++ manifest/common.js
@@ -117,41 +117,6 @@ function unixDaysAgo(uTime1, uTime2, days = 0, asString = true)
 }
 
 /*
-** Change a stylesheet
-*/
-function CCSStylesheetRuleStyle(stylesheet, selectorText, style, value)
-{
-  /* returns the value of the element style of the rule in the stylesheet
-  *  If no value is given, reads the value
-  *  If value is given, the value is changed and returned
-  *  If '' (empty string) is given, erases the value.
-  *  The browser will apply the default one
-  *
-  * string stylesheet: part of the .css name to be recognized, e.g. 'default'
-  * string selectorText: css selector, e.g. '#myId', '.myClass', 'thead td'
-  * string style: camelCase element style, e.g. 'fontSize'
-  * string value optionnal : the new value
-  */
-  var CCSstyle = undefined, rules;
-  for(var m in document.styleSheets){
-    if(document.styleSheets[m].href.indexOf(stylesheet) != -1){
-     rules = document.styleSheets[m][document.all ? 'rules' : 'cssRules'];
-     for(var n in rules){
-       if(rules[n].selectorText == selectorText){
-         CCSstyle = rules[n].style;
-         break;
-       }
-     }
-     break;
-    }
-  }
-  if(typeof value === 'undefined')
-    return CCSstyle[style]
-  else
-    return CCSstyle[style] = value
-}
-
-/*
 ** Number formatter
 */
 function numberWithCommas(x)
