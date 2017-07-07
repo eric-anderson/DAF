9f18c66d83106e6ef8236c427eb5fdb9c524c189
Eric <eric.anderson.nature.photos@gmail.com>
2017-07-06 21:49:45 -0700

    Sundry improvements since last round.

--- chrome-extension/ui.js
+++ chrome-extension/ui.js
@@ -25,15 +25,18 @@ function gotRaw(items) {
         console.log('rawData is not string?', items);
         return;
     }
-    var neighbours = processRaw(items);
-    if (neighbours === undefined) {
+    var parts = processRaw(items);
+    if (parts === undefined) {
+	console.log('error: processRaw failed');
         return;
     }
-    replaceDownloadElement('neighboursXML', neighbours);
+    replaceDownloadElement('neighboursXML', parts.neighbours);
+    replaceDownloadElement('giftsXML', parts.gifts);
     replaceDownloadElement('friendlistData', items.friendlistData);
 
-    var friends = parseNeighboursXML(neighbours);
-    items.derived = deriveState(items.derived, friends, items.lastDownload, items.playerID);
+    var friends = parseNeighboursXML(parts.neighbours);
+    var gifts = parseGiftsXML(parts.gifts);
+    items.derived = deriveState(items.derived, friends, gifts, items.lastDownload, items.playerID);
     console.log('derived', items.derived);
     replaceDownloadElement('derivedJSON', JSON.stringify(items.derived));
     chrome.storage.local.set({derived: items.derived});
@@ -69,7 +72,13 @@ function processRaw(items) {
         return undefined;
     }
 
-    return neighbours;
+    var gifts = filterXML(items.rawData, 'un_gifts');
+    if (gifts === undefined) {
+	console.log("Internal error, unable to find un_gifts in raw data");
+	return undefined;
+    }
+    
+    return { neighbours: neighbours, gifts: gifts };
 }
 
 function replaceDownloadElement(name, data) {
@@ -98,7 +107,7 @@ function filterXML(raw, part) {
 function parseNeighboursXML(neighbours) {
     var parser = new DOMParser();
     var xmlDoc = parser.parseFromString(neighbours, 'text/xml');
-    console.log('xmlDoc', xmlDoc);
+    console.log('neighbours.xmldoc', xmlDoc);
     var items = xmlDoc.getElementsByTagName('item');
     var friends = [];
     var columns = [];
@@ -134,6 +143,33 @@ function friendsToTSV(columns, friends) {
     }
     tsv.push('');
     replaceDownloadElement('neighboursTSV', tsv.join('\n'));
+}
+
+function parseGiftsXML(gifts) {
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(gifts, 'text/xml');
+    console.log('gifts.xmlDoc', xmlDoc);
+    var items = xmlDoc.getElementsByTagName('item');
+    var gift_received = {};
+    for (var i = 0; i < items.length; i++) {
+        var g = items.item(i);
+	var sender_id, gift_id;
+        for (var j = 0; j < g.childNodes.length; j++) {
+            var n = g.childNodes.item(j);
+	    if (n.nodeName == 'sender_id') {
+		sender_id = parseInt(n.innerHTML);
+	    } else if (n.nodeName == 'gift_id') {
+		gift_id = parseInt(n.innerHTML);
+	    }
+	}
+	if (sender_id && gift_id) {
+	    gift_received[sender_id] = gift_id;
+	}
+    }
+
+    console.log('gift_received', gift_received);
+
+    return gift_received;
 }
 
 var selectToFn = {
@@ -180,12 +216,12 @@ function selectActive(f, d) {
 function selectMiscTest(f, d) {
     var now = Math.floor(Date.now()/1000);
     var rec_gift = parseInt(f.rec_gift);
-    if (rec_gift < now - 4 * 24 * 3600) { // 4 days ago
-        if (d.gifts[2] >= 9) {
-            return true;
-        }
+    var back = d.giftReceived.length > 0 ? d.giftReceived[d.giftReceived.length-1] : 0;
+    if (rec_gift != 0 || d.gifts[0] < 5 || (now - back) > 3*86400) {
+	return false;
     }
-    return false;
+    console.log(d.name, d.gifts[0], '@', now - back, d, f);
+    return true;
 }
 
 function selectNewFriend(f, d) {
@@ -330,7 +366,7 @@ function makeGodChildrenSpan(sClass, text) {
     return span;
 }
 
-function deriveState(derived, inFriends, lastDownloadMS, playerID) {
+function deriveState(derived, inFriends, gifts, lastDownloadMS, playerID) {
     var lastUTS = parseInt((lastDownloadMS/1000).toFixed(0)); // unix time stamp
     derived = deriveInit(derived);
 
@@ -338,6 +374,11 @@ function deriveState(derived, inFriends, lastDownloadMS, playerID) {
         return derived;
     }
 
+    if (lastUTS < (Date.now() / 1000 - 86400)) {
+	console.log("ERROR, lastUTS more than a day ago?!", lastUTS);
+	return;
+    }
+    
     if (inFriends[inFriends.length-1].uid == playerID && inFriends[inFriends.length-2].uid == 1) {
         // ok, Player & Mr. Bill
     } else {
@@ -348,7 +389,7 @@ function deriveState(derived, inFriends, lastDownloadMS, playerID) {
     var friends = inFriends.slice(0, -2);
 
     deriveFirstLastSeen(derived, friends, lastUTS);
-    deriveGiftTracking(derived, friends, lastUTS);
+    deriveGiftTracking(derived, friends, gifts, lastUTS);
 
     return derived;
 }
@@ -377,6 +418,9 @@ function deriveFirstLastSeen(derived, friends, lastUTS) {
             console.log('first time seeing', f);
         }
         var d = derived.friends[f.uid];
+	if (d.firstSeen == 0) {
+	    d.firstSeen = lastUTS;
+	}
         if (d.hasOwnProperty('lastSeen')) {
             if (!d.reappearCount) {
                 d.reappearCount = 0;
@@ -387,6 +431,9 @@ function deriveFirstLastSeen(derived, friends, lastUTS) {
             console.log('friend reappeared', d.reappearCount, 'times', f);
         }
         d.name = getName(f);
+	if (d.name == 'Diggy Archer') {
+	    console.log(d);
+	}
     }
 
     for (var id in derived.friends) {
@@ -401,7 +448,7 @@ function deriveFirstLastSeen(derived, friends, lastUTS) {
     }
 }
 
-function deriveGiftTracking(derived, friends, lastUTS) {
+function deriveGiftTracking(derived, friends, gifts, lastUTS) {
     var prevUTS = derived.giftTracked.length == 0 ? 0 : derived.giftTracked[derived.giftTracked.length - 1];
 
     if (prevUTS == lastUTS) {
@@ -409,7 +456,7 @@ function deriveGiftTracking(derived, friends, lastUTS) {
         console.log('no deriving to do');
         return;
     }
-    deriveGiftTrackingReceived(derived, friends, prevUTS, lastUTS);
+    deriveGiftTrackingReceived(derived, friends, gifts, prevUTS, lastUTS);
     deriveGiftTrackingSummary(derived, lastUTS);
     var recentWarn = /^At \w+ Jun/;
     for (var id in derived.friends) {
@@ -426,11 +473,31 @@ function deriveGiftTracking(derived, friends, lastUTS) {
     derived.giftTracked.push(lastUTS);
 }
 
-function deriveGiftTrackingReceived(derived, friends, prevUTS, lastUTS) {
+function deriveGiftTrackingReceived(derived, friends, gifts, prevUTS, lastUTS) {
+    var count = { noActiveNoRec: 0, noActiveRec: 0, activeNoRec: 0, activeRec: 0, noRec: 0, rec: 0, noActive: 0, active: 0, delayedGift: 0, outOfOrderGift: 0, noGifts: 0, outOfOrderSeen: 0, outOfOrderNotSeen: 0, properOrderGift: 0 };
+    var fUid = {};
+    // Thu 12:30 -- no sends; noActiveNoRec 2182 ; noActiveRec 684 ; activeNoRec 680 ; activeRec 1300
+    //           -- identical on reload
+    // after accept --        noActiveNoRec 2723 ; noActiveRec 1786 ; activeNoRec 139 ; activeRec 198
+    //     14:01 -- gifts 566 ; noActiveNoRec 2584 ; noActiveRec 1702 ; activeNoRec 278 ; activeRec 282
+    // next day       Final counts: gifts 2000 ; noActiveNoRec 1418 ; noActiveRec 1448 ; activeNoRec 1444 ; activeRec 536 ; rec 1984 ; noRec 2862 ; active 1980 ; noActive 2866 ; giftNotFriend 20
+    // after accept   Final counts: gifts 1981 ; noActiveNoRec 2098 ; noActiveRec 793 ; activeNoRec 764 ; activeRec 1191 ; rec 1984 ; noRec 2862 ; active 1955 ; noActive 2891 ; giftNotFriend 26
+    // slightly later Final counts: gifts 2000 ; noActiveNoRec 2091 ; noActiveRec 781 ; activeNoRec 771 ; activeRec 1203 ; rec 1984 ; noRec 2862 ; active 1974 ; noActive 2872 ; giftNotFriend 26
+    // evening        Final counts: gifts 1589 ; noActiveNoRec 1795 ; noActiveRec 1478 ; activeNoRec 1067 ; activeRec 506 ; rec 1984 ; noRec 2862 ; active 1573 ; noActive 3273 ; giftNotFriend 16
+    // saturday       Final counts: gifts 2000 ; noActiveNoRec 2048 ; noActiveRec 829 ; activeNoRec 813 ; activeRec 1154 ; rec 1983 ; noRec 2861 ; active 1967 ; noActive 2877 ; giftNotFriend 33
+    // sat evening    Final counts: gifts 2000 ; noActiveNoRec 2048 ; noActiveRec 829 ; activeNoRec 813 ; activeRec 1154 ; rec 1983 ; noRec 2861 ; active 1967 ; noActive 2877 ; giftNotFriend 33
+    // sun            Final counts: gifts 2000 ; noActiveNoRec 2002 ; noActiveRec 869 ; activeNoRec 859 ; activeRec 1114 ; rec 1983 ; noRec 2861 ; active 1973 ; noActive 2871 ; giftNotFriend 27
+    // mon            Final counts: gifts 2000 ; noActiveNoRec 1546 ; noActiveRec 1325 ; activeNoRec 1316 ; activeRec 658 ; rec 1983 ; noRec 2862 ; active 1974 ; noActive 2871 ; giftNotFriend 26
+    // mon2           Final counts: gifts 2000 ; noActiveNoRec 2036 ; noActiveRec 831 ; activeNoRec 826 ; activeRec 1152 ; rec 1983 ; noRec 2862 ; active 1978 ; noActive 2867 ; giftNotFriend 22
+    // mon3           Final counts: gifts 172 ; noActiveNoRec 2772 ; noActiveRec 1902 ; activeNoRec 90 ; activeRec 81 ; rec 1983 ; noRec 2862 ; active 171 ; noActive 4674 ; giftNotFriend 1
+    console.log('processing', friends.length, 'friends with', Object.keys(gifts).length,'gifts');
+    var delayedGift = [];
     for (var i = 0; i < friends.length; i++) {
         var f = friends[i];
         var d = derived.friends[f.uid];
-        
+	delete d.warning;  // warnings broken right now
+	fUid[f.uid] = true;
+
         if (!d.giftTrackStart) {
             d.giftTrackStart = lastUTS;
         }
@@ -439,6 +506,59 @@ function deriveGiftTrackingReceived(derived, friends, prevUTS, lastUTS) {
         }
         var rec_gift = parseInt(f.rec_gift);
         var back = d.giftReceived.length == 0 ? 0 : d.giftReceived[d.giftReceived.length-1];
+	if (rec_gift == 0) {
+	    count.noRec++;
+	    if (back == 0) {
+		count.noGifts++;
+	    } else if (gifts[f.uid]) {
+		count.active++;
+		count.activeNoRec++;
+		// console.log('active no rec', f, d, 'gift', rec_gift, back, 'ts', prevUTS, lastUTS);
+		if (d.lastGiftId == gifts[f.uid]) {
+		    // console.log('duplicate gift id');
+		    rec_gift = back;
+		    f.rec_gift = back;
+		} else {
+		    if (d.lastGiftId && d.lastGiftId > gifts[f.uid]) {
+			// console.log('old? gift id', d.lastGiftId, gifts[f.uid], f, d);
+		    } else {
+			// console.log('new gift id', d.lastGiftId, gifts[f.uid], f, d);
+		    }
+
+		    d.lastGiftId = gifts[f.uid];
+		    rec_gift = lastUTS;
+		    f.rec_gift = lastUTS;
+		}
+	    } else {
+		count.noActive++;
+		count.noActiveNoRec++;
+		// no rec_gift, so use the tracking data
+		rec_gift = back;
+		f.rec_gift = back;
+	    }
+	} else {
+	    count.rec++;
+	    if (gifts[f.uid]) {
+		count.active++;
+		count.activeRec++;
+		// got rec_gift and active gift
+		if (!d.lastGiftId || gifts[f.uid] > d.lastGiftId) {
+		    d.lastGiftId = gifts[f.uid];
+		}
+	    } else {
+		count.noActive++;
+		count.noActiveRec++;
+		// console.log('got rec_gift, missing gift entry', f);
+	    }
+	}
+	if (rec_gift == 0) {
+	    if (back == 0) {
+		continue;
+	    }
+	    console.log('did not fix', rec_gift, back, f, d, gifts[f.uid]);
+	    throw "FIXME";
+	}
+
         if (back == rec_gift) {
             // unchanged; ignore
         } else {
@@ -446,11 +566,17 @@ function deriveGiftTrackingReceived(derived, friends, prevUTS, lastUTS) {
             // of cases where the timestamp for the downloaded data is a little
             // behind the timestamp we store, and so we see occasional out of
             // orders that aren't real.
-            if (rec_gift < prevUTS - 120) {
-                d.warning = 'At ' + (new Date()).toString() + ' Delayed gift notification ' + rec_gift + ' < ' + prevUTS + ' by ' + (prevUTS - rec_gift);
-                console.log('CrossCheck',f,d,d.warning);
-            }
             if (back < rec_gift) {
+		if (rec_gift < prevUTS - 120) {
+                    d.warning = 'At ' + (new Date()).toString() + ' uid=' + f.uid + ' Delayed gift notification prev=' + back + ' cur=' + rec_gift + ' < prevUTS=' + prevUTS + ' by ' + (prevUTS - rec_gift) + '; curUTS=' + lastUTS;
+                    if (count.delayedGift < 5) {
+			console.log('DelayedGift',f,d,d.warning);
+		    }
+		    count.delayedGift++;
+		    delayedGift.push(prevUTS - rec_gift);
+		} else {
+		    count.properOrderGift++;
+		}
                 d.giftReceived.push(rec_gift); // new
             } else {
                 d.warning = 'At ' + (new Date()).toString() + ' Out of order gifts: ' + back + ' > ' + rec_gift;
@@ -458,13 +584,53 @@ function deriveGiftTrackingReceived(derived, friends, prevUTS, lastUTS) {
                     d.warning = d.warning + ' not seen before';
                     d.giftReceived.push(rec_gift);
                     d.giftReceived.sort(function(a,b) { return a - b; });
+		    count.outOfOrderNotSeen++;
                 } else {
                     d.warning = d.warning + ' already seen';
+		    count.outOfOrderSeen++;
                 }
+		if (count.outOfOrderGift < 5) {
+		    console.log('OutOfOrderGift', f,d,d.warning);
+		}
+		count.outOfOrderGift++;
             }
             // console.log('id',f.uid,'rg',rec_gift, d);
         }
     }
+    var giftNotFriend = 0;
+    for (var i in gifts) {
+	if (!gifts.hasOwnProperty(i)) {
+	    continue;
+	}
+	if (fUid[i]) {
+	    // console.log('have friend', i, 'derived', derived.friends[i]);
+	} else {
+	    ++giftNotFriend;
+	    console.log('missing friend from ', i, ' derived ', derived.friends[i]);
+	} 
+    }
+    console.log('Final counts: gifts', Object.keys(gifts).length, '; noActiveNoRec', count.noActiveNoRec, '; noActiveRec', count.noActiveRec, '; activeNoRec', count.activeNoRec, '; activeRec', count.activeRec, '; rec', count.rec, '; noRec', count.noRec, '; active', count.active, '; noActive', count.noActive, '; giftNotFriend', giftNotFriend, '; delayedGift', count.delayedGift, '; outOfOrderGift', count.outOfOrderGift, '; noGifts', count.noGifts, '; outOfOrderSeen', count.outOfOrderSeen, '; outOfOrderNotSeen', count.outOfOrderNotSeen, '; properOrderGift', count.properOrderGift);
+    if (delayedGift.length > 0) {
+	var n = delayedGift.length;
+        delayedGift.sort(function(a,b) { return a - b; });
+	// not actually correct quantiles but close enough.
+	// Final counts: gifts 64 ; noActiveNoRec 393 ; noActiveRec 4384 ; activeNoRec 0 ; activeRec 62 ; rec 4446 ; noRec 398 ; active 62 ; noActive 4777 ; giftNotFriend 2 ; delayedGift 76 ; outOfOrderGift 755 ; noGifts 5 ; outOfOrderSeen 755 ; outOfOrderNotSeen 0
+	// Delayed gift: 10%: 30165 ; 50%: 84376 ; 90%: 91395 ; 95%: 92298 ; max: 92921
+	// Final counts: gifts 329 ; noActiveNoRec 395 ; noActiveRec 4120 ; activeNoRec 0 ; activeRec 324 ; rec 4444 ; noRec 400 ; active 324 ; noActive 4515 ; giftNotFriend 5 ; delayedGift 231 ; outOfOrderGift 414 ; noGifts 5 ; outOfOrderSeen 414 ; outOfOrderNotSeen 0
+	// Delayed gift: 10%: 38650 ; 50%: 76842 ; 90%: 86648 ; 95%: 88498 ; max: 91878	
+	// Final counts: gifts 1697 ; noActiveNoRec 380 ; noActiveRec 2780 ; activeNoRec 0 ; activeRec 1676 ; rec 4456 ; noRec 386 ; active 1676 ; noActive 3160 ; giftNotFriend 21 ; delayedGift 1404 ; outOfOrderGift 0 ; noGifts 6 ; outOfOrderSeen 0 ; outOfOrderNotSeen 0 ; properOrderGift 132
+	// Delayed gift: 10%: 45583 ; 50%: 67747 ; 90%: 83035 ; 95%: 85738 ; max: 92757
+	// Final counts: gifts 246 ; noActiveNoRec 378 ; noActiveRec 4213 ; activeNoRec 0 ; activeRec 245 ; rec 4458 ; noRec 384 ; active 245 ; noActive 4591 ; giftNotFriend 1 ; delayedGift 264 ; outOfOrderGift 0 ; noGifts 6 ; outOfOrderSeen 0 ; outOfOrderNotSeen 0 ; properOrderGift 20
+	// Delayed gift: 10%: 48792 ; 50%: 82592 ; 90%: 89554 ; 95%: 90486 ; max: 92987
+	// Final counts: gifts 472 ; noActiveNoRec 379 ; noActiveRec 3989 ; activeNoRec 0 ; activeRec 469 ; rec 4458 ; noRec 384 ; active 469 ; noActive 4368 ; giftNotFriend 3 ; delayedGift 231 ; outOfOrderGift 0 ; noGifts 5 ; outOfOrderSeen 0 ; outOfOrderNotSeen 0 ; properOrderGift 18
+	// Delayed gift: 10%: 53036 ; 50%: 83222 ; 90%: 89385 ; 95%: 90393 ; max: 92622
+	// Final counts: gifts 1586 ; noActiveNoRec 375 ; noActiveRec 2892 ; activeNoRec 0 ; activeRec 1570 ; rec 4462 ; noRec 380 ; active 1570 ; noActive 3267 ; giftNotFriend 16 ; delayedGift 897 ; outOfOrderGift 0 ; noGifts 5 ; outOfOrderSeen 0 ; outOfOrderNotSeen 0 ; properOrderGift 114
+	// Delayed gift: 10%: 38947 ; 50%: 72665 ; 90%: 86046 ; 95%: 88354 ; max: 92743
+
+	console.log('Delayed gift: 10%:', delayedGift[Math.floor(n * 0.1)], '; 50%:', delayedGift[Math.floor(n* 0.5)], '; 90%:',
+		    delayedGift[Math.floor(n*0.9)], '; 95%:', delayedGift[Math.floor(n*0.95)], '; max:', delayedGift[n-1]);
+    }
+//    throw "fail";
 }
 
 function deriveGiftTrackingSummary(derived, lastUTS) {
@@ -495,7 +661,7 @@ function deriveGiftTrackingSummary(derived, lastUTS) {
         for(; i >= 0 && e[i] < 0; i--) { }
         i++;
         if (i < e.length) {
-            console.log('splice from', i, e[i]);
+            // console.log('splice from', i, e[i]);
             e.splice(i, e.length - i);
         }
     });
