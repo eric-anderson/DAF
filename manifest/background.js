a2443875ab5f51025b14308ef21bad5c3afb3c1e
monty68 <monty-68@hotmail.com>
2017-07-15 06:36:40 +0100

    Load syncScript on preference change

--- manifest/background.js
+++ manifest/background.js
@@ -82,6 +82,9 @@ chrome.storage.onChanged.addListener(function (changes, area) {
 
             // Anything to do per specific preference change?
             switch (key) {
+                case 'gameSync':
+                    daGame.syncScript();
+                    break;
                 default: break;
             }
         }
