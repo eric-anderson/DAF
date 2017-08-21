3c44f003ea872e80974ef5028d0df66ed4657d3f
monty68 <monty-68@hotmail.com>
2017-08-21 12:34:34 +0100

    Match loot to chest/ring

--- manifest/tabs/calculators/g_ring.js
+++ manifest/tabs/calculators/g_ring.js
@@ -30,15 +30,15 @@ var guiTabs = (function(self) {
      ** @Public - Display Ring Loot
      */
     self.ringLoot = function(tid, reason, mines, tab) {
-        var region = parseInt(bgp.daGame.daUser.region);
-        var level = parseInt(bgp.daGame.daUser.level);
-        var div = tab.html;
+        let region = parseInt(bgp.daGame.daUser.region);
+        let level = parseInt(bgp.daGame.daUser.level);
+        let div = tab.html;
         div.innerHTML = '';
 
         for (var m = 0; m < mines.length; m++) {
-            var mine = bgp.daGame.mineDetails(mines[m]);
-            var rows = 0;
-            var html = [];
+            let mine = bgp.daGame.mineDetails(mines[m]);
+            let rows = 0;
+            let html = [];
 
             //console.log(mine);
 
@@ -60,22 +60,46 @@ var guiTabs = (function(self) {
 
             Object.keys(mine.floors).forEach(function(fid) {
                 if (mine.floors[fid].hasOwnProperty('loot')) {
+                    let chest = 0;
+                    let last = null;
                     html.push('<tbody id="', tid, m, '-tb-', fid, '">');
-                    Object.keys(mine.floors[fid].loot).forEach(function(lid) {
-                        var loot = mine.floors[fid].loot[lid];
-                        var coef = parseFloat(loot.cof);
-                        var min = parseInt(loot.min) + (coef != 0.0 ? Math.floor((level * coef) * loot.min) : 0);
-                        var max = parseInt(loot.max) + (coef != 0.0 ? Math.floor((level * coef) * loot.max) : 0);
-                        var avg = Math.floor((min + max) / 2);
+                    
+                    //console.log(mine);
 
-                        html.push('<tr>');
-                        html.push('<td>', loot.aid, '</td>');
-                        html.push('<td>', self.objectName(loot.typ, loot.oid), '</td>');
-                        html.push('<td>', numberWithCommas(min), '</td>');
-                        html.push('<td>', numberWithCommas(avg), '</td>');
-                        html.push('<td>', numberWithCommas(max), '</td>');
-                        html.push('</tr>');
-                        rows += 1;
+                    Object.keys(mine.floors[fid].loot).sort(function(a, b) {
+                        let ta = mine.floors[fid].loot[a];                       
+                        let tb = mine.floors[fid].loot[b];
+
+                        if(ta.tle[0] < tb.tle[0]) return -1;
+                        if(ta.tle[0] > tb.tle[0]) return 1;
+
+                        return mine.floors[fid].loot[a].oid - mine.floors[fid].loot[b].oid;
+
+                    }).forEach(function(lid) {
+                        let loot = mine.floors[fid].loot[lid];
+                        let coef = parseFloat(loot.cof);
+                        let min = parseInt(loot.min) + (coef != 0.0 ? Math.floor((level * coef) * loot.min) : 0);
+                        let max = parseInt(loot.max) + (coef != 0.0 ? Math.floor((level * coef) * loot.max) : 0);
+                        let avg = Math.floor((min + max) / 2);
+                        let tile = loot.tle[0];
+
+                        if (last != tile) {
+                            last = tile;
+                            chest = chest + 1;
+                        }
+                        
+                        //console.log(chest, loot.aid, loot.tle, tile, self.objectName(loot.typ, loot.oid));
+
+                        if (loot.typ != 'token') {
+                            html.push('<tr>');
+                            html.push('<td>', chest, '</td>');
+                            html.push('<td>', self.objectName(loot.typ, loot.oid), '</td>');
+                            html.push('<td>', numberWithCommas(min), '</td>');
+                            html.push('<td>', numberWithCommas(avg), '</td>');
+                            html.push('<td>', numberWithCommas(max), '</td>');
+                            html.push('</tr>');
+                            rows += 1;
+                        }
                     });
                     html.push('</tbody>');
                 }
